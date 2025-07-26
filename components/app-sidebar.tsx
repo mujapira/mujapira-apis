bbf86d9cdb8c8c6c59c1b0db0625aa6532badde0
mujapira <malmasmello2@gmail.com>
2025-07-26 13:47:03 -0300

    chore: update dependencies and add new packages for UI components and styling

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,97 @@
+"use client"
+
+import * as React from "react"
+import {
+  AudioWaveform,
+  Banana,
+  BookOpen,
+  Bot,
+  ClipboardCheck,
+  Code,
+  Command,
+  Frame,
+  GalleryVerticalEnd,
+  Layers,
+  Map,
+  PieChart,
+  Settings2,
+  SquareTerminal,
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavUser } from "@/components/nav-user"
+import { TeamSwitcher } from "@/components/team-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Mujapira",
+      logo: Banana,
+      plan: "Central de API's e mais",
+    },
+  ],
+  navMain: [
+    {
+      title: "Magic Utils",
+      url: "#",
+      icon: Layers,
+      isActive: false,
+      items: [
+        {
+          title: "Acessar",
+          url: "",
+        },
+        {
+          title: "Github",
+          url: "https://github.com/mujapira/magic-utils",
+        },
+      ],
+    },
+    {
+      title: "App Tarefas",
+      url: "#",
+      icon: ClipboardCheck,
+      isActive: false,
+      items: [
+        {
+          title: "Acessar",
+          url: "",
+        },
+        {
+          title: "Github",
+          url: "https://github.com/mujapira/magic-utils",
+        },
+      ],
+    }
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
---
+++ components/nav-main.tsx
@@ -0,0 +1,96 @@
+"use client"
+
+import { ChevronRight, GitGraph, SquareArrowUpRight, type LucideIcon } from "lucide-react"
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from "@/components/ui/sidebar"
+
+export function NavMain({
+  items,
+}: {
+  items: {
+    title: string
+    url: string
+    icon?: LucideIcon
+    isActive?: boolean
+    items?: {
+      title: string
+      url: string
+    }[]
+  }[]
+}) {
+  return (
+    <>
+
+
+      <SidebarGroup>
+        <SidebarGroupLabel>Projetos</SidebarGroupLabel>
+        <SidebarMenu>
+          {items.map((item) => (
+            <Collapsible
+              key={item.title}
+              asChild
+              defaultOpen={item.isActive}
+              className="group/collapsible"
+            >
+              <SidebarMenuItem>
+                <CollapsibleTrigger asChild>
+                  <SidebarMenuButton tooltip={item.title}>
+                    {item.icon && <item.icon />}
+                    <span>{item.title}</span>
+                    <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                  </SidebarMenuButton>
+                </CollapsibleTrigger>
+                <CollapsibleContent>
+                  <SidebarMenuSub>
+                    {item.items?.map((subItem) => (
+                      <SidebarMenuSubItem key={subItem.title}>
+                        <SidebarMenuSubButton asChild>
+                          <a href={subItem.url}>
+                            <span>{subItem.title}</span>
+                          </a>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    ))}
+                  </SidebarMenuSub>
+                </CollapsibleContent>
+              </SidebarMenuItem>
+            </Collapsible>
+          ))}
+        </SidebarMenu>
+      </SidebarGroup>
+
+      <SidebarGroup>
+        <SidebarGroupLabel>Sobre mim</SidebarGroupLabel>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuSubButton asChild>
+              <div>
+                <GitGraph />
+                <a target="_blank" href="https://github.com/mujapira/" className="flex flex-1 items-start">
+                  <span>GitHub</span>
+                </a>
+                <SquareArrowUpRight />
+              </div>
+            </SidebarMenuSubButton>
+
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarGroup>
+    </>
+
+  )
+}
