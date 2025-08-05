c75bb4a4b833c3cf07c821edf9e5fd1b505c73e0
mujapira <malmasmello2@gmail.com>
2025-08-04 23:04:15 -0300

    refactor: remove access token management and related interceptors from apiGateway

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,55 +1,6 @@
-import { refreshRequest } from "@/services/auth";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 
 export const apiGateway = axios.create({
   baseURL: "https://api.mujapira.com/",
-  withCredentials: true, // envia o cookie HttpOnly de refresh
+  withCredentials: true,
 });
-
-// estado em memória (não persiste em localStorage para evitar XSS)
-let accessToken: string | null = null;
-let isRefreshing = false;
-let refreshPromise: Promise<string> | null = null;
-
-// injetar access token nos requests
-apiGateway.interceptors.request.use((config) => {
-  if (accessToken) {
-    config.headers = config.headers ?? {};
-    config.headers["Authorization"] = `Bearer ${accessToken}`;
-  }
-  return config;
-});
-
-// interceptor de resposta para tentar refresh em 401
-apiGateway.interceptors.response.use(
-  (res) => res,
-  async (err: AxiosError) => {
-    if (err.response?.status === 401 && !err.config?.headers?.["x-retried"]) {
-      // evita loops
-      if (!isRefreshing) {
-        isRefreshing = true;
-        refreshPromise = refreshRequest()
-          .then((d) => {
-            accessToken = d.accessToken;
-            return accessToken;
-          })
-          .finally(() => {
-            isRefreshing = false;
-          });
-      }
-      try {
-        await refreshPromise;
-        // retry original request
-        const originalConfig = err.config!;
-        originalConfig.headers = originalConfig.headers ?? {};
-        originalConfig.headers["Authorization"] = `Bearer ${accessToken}`;
-        originalConfig.headers["x-retried"] = "1";
-        return apiGateway(originalConfig);
-      } catch {
-        // refresh falhou: cai para o erro original
-        return Promise.reject(err);
-      }
-    }
-    return Promise.reject(err);
-  }
-);
